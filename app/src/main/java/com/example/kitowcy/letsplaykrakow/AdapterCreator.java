f510a1616074c836027e0ba2b42a67261a25d2ad
VaccumMan <lukmar93@o2.pl>
2015-11-21 23:03:02 +0100

    added drawer closing

--- app/src/main/java/com/example/kitowcy/letsplaykrakow/AdapterCreator.java
+++ app/src/main/java/com/example/kitowcy/letsplaykrakow/AdapterCreator.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AdapterCreator {
-
     private static final String TAG = AdapterCreator.class.getSimpleName();
 
     private AdapterCreator() {
@@ -28,13 +27,37 @@ public class AdapterCreator {
 
         MaterialDrawerAdapter
                 mAdapter = new MaterialDrawerAdapter(context, getDataSet());       // Creating the Adapter of MyAdapter class(which we are going to see in a bit)
-       mAdapter.setOnClickListener(new MaterialDrawerAdapter.onItemClickListener() {
-           @Override
-           public void onClick(int position) {
-               Log.d(TAG, "clicked: " + position);
-           }
-       });
+        mAdapter.setOnClickListener(new MaterialDrawerAdapter.onItemClickListener() {
+            @Override
+            public void onClick(int position) {
+                Log.d(TAG, "clicked: " + position);
+                switch (position) {
+                    case -1:
+                        //// TODO: 21.11.15 splash screen
+                        switchTo(context, FragmentUnit.SPLASH);
+                        break;
+                    case 0:
+                        switchTo(context, FragmentUnit.MAP);
+                        break;
+                    case 1:
+                        switchTo(context, FragmentUnit.ROUTES);
+                        break;
+                    case 2:
+                        switchTo(context, FragmentUnit.PLACES);
+                        break;
+                    case 3:
+                        switchTo(context, FragmentUnit.ACHIEVEMENTS);
+                        break;
+                }
+                if (drawer != null)
+                    drawer.closeDrawers();
+            }
+        });
         return mAdapter;
+    }
+
+    private static void switchTo(AppCompatActivity context, FragmentUnit fragmentUnit) {
+        FragmentSwitcher.switchToFragment(context, FragmentUnit.ACHIEVEMENTS, R.id.activity_main_fragment_placeholder, null);
     }
 
 }
