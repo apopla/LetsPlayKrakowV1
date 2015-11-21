d4ed89bbaee85869c2a57d5bb492589edfbbc223
VaccumMan <lukmar93@o2.pl>
2015-11-21 18:38:41 +0100

    core app menu created

---
+++ app/src/main/java/com/example/kitowcy/letsplaykrakow/Constants.java
@@ -0,0 +1,7 @@
+package com.example.kitowcy.letsplaykrakow;
+
+/**
+ * Created by lukasz on 21.11.15.
+ */
+public class Constants {
+}
--- app/src/main/java/com/example/kitowcy/letsplaykrakow/FragmentUnit.java
+++ app/src/main/java/com/example/kitowcy/letsplaykrakow/FragmentUnit.java
@@ -19,19 +19,9 @@ import pl.sointeractive.smartcity.fragments.timetable.TimetableFragment;
  */
 public enum FragmentUnit {
 
-    JOURNEYS (0, MyJourneysFragment.class),
-    TIMETABLE (1, TimetableFragment.class),
-    CITYMAP (2, TimetableBusOperatorsFragment.class),
-    OFFERS (3, TestMyOffersFragment.class),
-    COMMUNITY (4, CommunityFragment.class),
-    PROFILE (5, ProfileHolderFragment.class),
-    SETTINGS(6, SettingsHolderFragment.class),
-    PROFILE_EDIT(7, ProfileEditFragment.class),
-    START_SCREEN(12, StartScreenFragment.class),
-    INFORMATION(13, InformationFragment.class),
-    TIMETABLE_BUS_OPERATORS(28, TimetableBusOperatorsFragment.class),
-    NOTIFICATIONS(100, AllNotificationsFragment.class),
-    BUS_STOP_LINES(31, StopLineFragment.class);
+    START (0, StartFragment.class),
+    SETTINGS (1, StartFragment.class),
+    BADGES(2, StartFragment.class);
 
     /**
      * Subclass of base Fragment class
