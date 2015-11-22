6c20cbe6c64077b2646ab2fa0990320bcf4b8123
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2015-11-22 08:07:51 +0100

    Routes Fragment

--- app/src/main/java/com/example/kitowcy/letsplaykrakow/FragmentUnit.java
+++ app/src/main/java/com/example/kitowcy/letsplaykrakow/FragmentUnit.java
@@ -6,6 +6,7 @@ import com.example.kitowcy.letsplaykrakow.entities.fragments.AchievementsFragmen
 import com.example.kitowcy.letsplaykrakow.entities.fragments.GoogleMapFragment;
 import com.example.kitowcy.letsplaykrakow.entities.fragments.GridViewFragment;
 import com.example.kitowcy.letsplaykrakow.entities.fragments.PlacesFragment;
+import com.example.kitowcy.letsplaykrakow.entities.fragments.RoutesFragment;
 import com.example.kitowcy.letsplaykrakow.entities.fragments.SchematFragment;
 import com.example.kitowcy.letsplaykrakow.entities.fragments.StartFragment;
 
@@ -14,8 +15,8 @@ public enum FragmentUnit {
 
     SPLASH(-1, SchematFragment.class),
     MAP(0, GoogleMapFragment.class),
-    ROUTES(1, StartFragment.class),
-    ACHIEVEMENTS(2, AchievementsFragment.class),
+    ROUTES(1, RoutesFragment.class),
+    ACHIEVEMENTS(2, GridViewFragment.class),
     RECYCLER(3, GridViewFragment.class),
 
     PLACES(4, PlacesFragment.class);
